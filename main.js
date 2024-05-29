ed7aa560e02ad9151be9c46254467326705a4ea9
gabriel-05-dev <g.luchita@protonmail.com>
2024-05-29 20:35:17 +0100

    added evaluate event listener and parseint for numbers funtion

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 numbers = document.querySelectorAll('.number');
+operators = document.querySelectorAll('.operator');
+evaluate = document.querySelector('.evaluate');
+
 display = document.querySelector('.displayContent');
 let value1 = 0;
+let value2 = 0;
+let operator = '';
 
 numbers.forEach(function(button) {
     button.addEventListener("click", function() {
-    value1 += button.innerText;
     display.innerText += button.innerText;
+    value1 = parseFloat(display.innerText);
     });
   });
+
+
+//event listeners for operators.
+operators.forEach(function(operator) {
+  operator.addEventListener("click", function() {
+    console.log(operator.innerText);
+  });
+});
+
+evaluate.addEventListener('click', () => {
+  operate(value1, value2, operator);
+});
+
+
+
+//if statements to check operator
+function operate(value1, value2, operator) {
+  console.log(operator);
+};
